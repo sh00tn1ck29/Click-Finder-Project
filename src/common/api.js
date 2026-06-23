7e606d4ff4377798c594b7a56f50f7587e2ff629
sh00tn1ck29 <maksimsavrygin21@gmail.com>
2026-06-23 23:55:03 +0200

    Update api.js

--- src/common/api.js
+++ src/common/api.js
@@ -1,15 +1,22 @@
 const USER_DATA_API = 'https://appco-snowy.vercel.app/api/users';
 const STATS_API = 'https://appco-snowy.vercel.app/api/users/statistics';
 
-export const fetchUsers = () => {
-  return fetch(USER_DATA_API)
+export const fetchUsers = (page, limit) => {
+  return fetch(`${USER_DATA_API}?page=${page}&rowsPerPage=${limit}`)
     .then((res) =>
       res.ok ? res.json() : Promise.reject('Ошибка пользователей'),
     )
-    .then((result) => result.data || [])
+    .then((result) => {
+      const usersList = result.data || (Array.isArray(result) ? result : []);
+      const total = result.totalPages || 63;
+      return {
+        users: usersList,
+        totalPages: total,
+      };
+    })
     .catch((err) => {
       console.error(err);
-      return [];
+      return { users: [], totalPages: 63 };
     });
 };
 
