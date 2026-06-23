f96909586052ff3e7cbae64d39b3a7b84a279953
sh00tn1ck29 <maksimsavrygin21@gmail.com>
2026-06-24 00:20:08 +0200

    Update stats.js

--- src/scripts/stats.js
+++ src/scripts/stats.js
@@ -2,6 +2,11 @@ import { fetchUsers } from '../common/api.js';
 import { toggleLoader } from './dom.js';
 import { renderPageData } from './render.js';
 
+if (window.location.pathname.includes('/index.html')) {
+  const cleanPath = window.location.pathname.replace('/index.html', '/');
+  window.history.replaceState({}, document.title, cleanPath + window.location.search);
+}
+
 let currentPage = 1;
 const limit = 16;
 
