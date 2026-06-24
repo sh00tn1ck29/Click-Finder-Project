793ab77a2b8c3301edbe32ecf21e9103471317c6
sh00tn1ck29 <maksimsavrygin21@gmail.com>
2026-06-24 11:01:12 +0200

    Update stats.js

--- src/scripts/stats.js
+++ src/scripts/stats.js
@@ -2,11 +2,6 @@ import { fetchUsers } from '../common/api.js';
 import { toggleLoader } from './dom.js';
 import { renderPageData } from './render.js';
 
-if (window.location.pathname.includes('/index.html')) {
-  const cleanPath = window.location.pathname.replace('/index.html', '/');
-  window.history.replaceState({}, document.title, cleanPath + window.location.search);
-}
-
 let currentPage = 1;
 const limit = 16;
 
@@ -27,6 +22,14 @@ const handlePageChange = (newPage) => {
 };
 
 const init = () => {
+  if (window.location.pathname.includes('/index.html')) {
+    const cleanPath = window.location.pathname.replace('/index.html', '/');
+    window.history.replaceState(
+      {},
+      document.title,
+      cleanPath + window.location.search,
+    );
+  }
   loadPageData(currentPage);
 };
 
