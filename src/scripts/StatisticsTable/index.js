9c45bc3937703f4a40ecc93d8f2195a63dc36c85
sh00tn1ck29 <maksimsavrygin21@gmail.com>
2026-06-25 19:05:52 +0200

    Create index.js

---
+++ src/scripts/StatisticsTable/index.js
@@ -0,0 +1,59 @@
+import { fetchAllUsersStats } from '../../common/gateways/api.js';
+import { renderPagination } from '../Pagination/index.js';
+
+const tableBody = document.querySelector('#table-body');
+const loader = document.querySelector('#linear-progress');
+
+export const toggleLoader = (show) => {
+  if (!loader) return;
+  if (show) {
+    loader.classList.remove('hidden');
+  } else {
+    loader.classList.add('hidden');
+  }
+};
+
+export const renderPageData = (
+  usersForPage,
+  totalPages,
+  currentPage,
+  onPageChange,
+) => {
+  const pageUserIds = usersForPage.map((u) => u.id);
+
+  fetchAllUsersStats(pageUserIds).then((statsList) => {
+    if (!tableBody) return;
+    tableBody.innerHTML = '';
+
+    usersForPage.forEach((user) => {
+      const userStatsRecords = statsList.filter(
+        (el) => Number(el.user_id || el.userId || el.id) === Number(user.id),
+      );
+
+      const clicks = userStatsRecords.reduce(
+        (sum, el) => sum + Number(el.clicks || 0),
+        0,
+      );
+      const views = userStatsRecords.reduce(
+        (sum, el) => sum + Number(el.page_views || 0),
+        0,
+      );
+
+      const row = document.createElement('tr');
+      row.innerHTML = `
+        <td>${user.id}</td>
+        <td>${user.first_name}</td>
+        <td>${user.last_name}</td>
+        <td>${user.email}</td>
+        <td>${user.gender}</td>
+        <td>${user.ip_address}</td>
+        <td>${clicks}</td>
+        <td>${views}</td>
+      `;
+      tableBody.append(row);
+    });
+
+    renderPagination(totalPages, currentPage, onPageChange);
+    toggleLoader(false);
+  });
+};
